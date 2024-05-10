57b6ab0fa7e5f81875d3698ad7cb548830b87a4c
taham8875 <taha.m8875@gmail.com>
2024-05-10 11:48:59 +0300

    feat: Add search bar component

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MySearchBar extends StatelessWidget {
+  const MySearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: const Row(
+        children: [
+          Icon(Icons.search),
+          SizedBox(width: 10),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
